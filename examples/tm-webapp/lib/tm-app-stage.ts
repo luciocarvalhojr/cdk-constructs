c3fbdf06a9aee5a9f7002c74af40d41182459467
tm-lcarvalho <lucio.carvalho@toumoro.com>
2024-06-25 15:22:23 -0400

    add account info

--- examples/tm-webapp/lib/tm-app-stage.ts
+++ examples/tm-webapp/lib/tm-app-stage.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from "constructs";
 import { TmVpcbaseStack } from './tm-vpc-base-stack';
+import 
 
 export class TmPipelineAppStage extends cdk.Stage {
 
@@ -8,9 +9,11 @@ export class TmPipelineAppStage extends cdk.Stage {
       super(scope, id, props);
 
       const caCentral1Env = {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
         region: 'ca-central-1',
       }
       const caWest1Env = {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
         region: 'ca-west-1',
       }
       
